b191d4b2358b0ad3ff2a68136e92994d0c56f45e
Kenji <kenji.arakaki93@gmail.com>
2022-05-20 09:14:28 -0300

    basic types

--- tipos-basicos/index.js
+++ tipos-basicos/index.js
@@ -1,1 +1,17 @@
 "use strict";
+// Booleans //
+let isDev = true;
+let isSenior = false;
+console.log('Booleans', { isDev }, { isSenior });
+// Numbers //
+let age = 29;
+console.log('Numbers', { age });
+// Strings //
+let devName = 'Kenji';
+let devSurname = "Arakaki";
+let devJob = `Frontend`;
+console.log('Strings', { devName }, { devSurname }, { devJob });
+// Any //
+let workComputer = 123;
+workComputer = 'Macbook pro';
+console.log('Any', { workComputer });
---
+++ tipos-basicos/index.ts
@@ -0,0 +1,27 @@
+// Booleans //
+
+let isDev: boolean = true;
+let isSenior: boolean = false;
+
+console.log('Booleans', {isDev}, {isSenior});
+
+// Numbers //
+
+let age: number = 29;
+
+console.log('Numbers',{age});
+
+// Strings //
+
+let devName: string = 'Kenji';
+let devSurname: string = "Arakaki";
+let devJob: string = `Frontend`;
+
+console.log('Strings', {devName}, {devSurname}, {devJob});
+
+// Any //
+
+let workComputer: any = 123;
+workComputer = 'Macbook pro';
+
+console.log('Any', {workComputer});
